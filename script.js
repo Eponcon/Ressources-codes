d9f965e0cd88761e685b0fbf88570db8d63adf3e
Eponcon <poncon.elodie@gmail.com>
2017-10-26 16:17:30 +0200

    fin menu burger

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ var iconBurger = document.querySelector('.header-icone');
 var liensMenu = document.querySelector('.menu');
 var blcIcon = document.querySelector("#nav-toggle" );
 var lienItem = document.querySelectorAll('.lien-menu');
-console.log(lienItem);
 
 iconBurger.addEventListener('click', toggleMenu);
 
+for(i=0;i<lienItem.length; i++){
+    lienItem[i].addEventListener("click", toggleMenu);    
+}
+
+
+//affiche les liens du menu//
 function toggleMenu(){
     liensMenu.classList.toggle('visible');
-}
-
-for(i=0;i<lienItem.length; i++){
-    lienItem[i].addEventListener("click", toggleMenu);
-}
-
-blcIcon.addEventListener("click", burgerCroix);
-function burgerCroix(){
-    this.classList.toggle( "active" );
+    blcIcon.classList.toggle("active");
 }
 
